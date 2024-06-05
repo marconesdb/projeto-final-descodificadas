3318a490a91715f998a65ddec4f1dc6a75bce88a
marconebrito <marcone.brito@elystech.com.br>
2024-06-04 23:27:41 -0300

    Add de eventos no carrinho de compras

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+document.getElementById("botaoComprar").addEventListener("click", function() {
+    // Mostrar a caixa de alerta personalizada
+    document.getElementById("alertMessage").textContent = "Item adicionado ao carrinho!";
+    document.getElementById("alertContainer").style.display = "flex";
 
-// Script para caixa de alerta personalizada
+    // Incrementar a contagem de itens no carrinho
+    let cartCounter = document.getElementById("cartCounter");
+    let itemCount = parseInt(cartCounter.textContent);
+    itemCount++;
+    cartCounter.textContent = itemCount;
 
-        document.getElementById("botaoComprar").addEventListener("click", function() {
-            // Mostrar a caixa de alerta personalizada
-            document.getElementById("alertMessage").textContent = "Item adicionado ao carrinho!";
-            document.getElementById("alertContainer").style.display = "flex";
-        });
+    // Mostrar o contador se estiver oculto
+    if (itemCount > 0) {
+        cartCounter.style.display = "inline";
+    }
+});
 
-        document.getElementById("closeAlertButton").addEventListener("click", function() {
-            // Fechar a caixa de alerta personalizada
-            document.getElementById("alertContainer").style.display = "none";
-        });
-    
-// Fim do Script para caixa de alerta personalizada
+document.getElementById("closeAlertButton").addEventListener("click", function() {
+    // Fechar a caixa de alerta personalizada
+    document.getElementById("alertContainer").style.display = "none";
+});
+
+
+// Mensagem de itens do Carinho
+
+function showCartMessage() {
+    var cartCounter = document.getElementById("cartCounter").innerText;
+    var message = "Seu carrinho tem " + cartCounter + " item(s).";
+    alert(message);
+}
